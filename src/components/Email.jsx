90c86d7092714d5f509f2f1ef997bb7db3260e80
Manuel Pérez Jurado <perezjuradomanuel@gmail.com>
2023-12-11 21:12:05 +0100

    redux toolkit

--- src/components/Email.jsx
+++ src/components/Email.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { changeEmail } from '../redux/userSlice'
 
 const Email = () => {
+
+const dispatch = useDispatch()
+
+
+  const email = useSelector((state) => state.user.email)
+  
+  const handleChange = (e) => {
+    dispatch(changeEmail(e.target.value));
+  }
   return (
-    <div>Email</div>
+    <input type='email' value={email} placeholder='Email...' onChange={handleChange} />
   )
 }
 
